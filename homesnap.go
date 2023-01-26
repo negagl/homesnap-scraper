202d524c131fa24dcd0317d0a53719b3d7214f2a
negagl <nega_gl@outlook.com>
2023-01-26 15:08:43 -0500

    Connecting with GetDetails endpoint

--- homesnap.go
+++ homesnap.go
@@ -5,23 +5,25 @@ import (
 	"fmt"
 	"io"
 	"net/http"
+	"strconv"
 	"strings"
 )
 
 type House struct {
-	PropertyID int    `json:"propertyid"`
-	Address    string `json:"address"`
-	City       string `json:"city"`
-	State      string `json:"state"`
-	Zipcode    string `json:"zipcode"`
-	Beds       string `json:"beds"`
-	Baths      string `json:"baths"`
-	Sqft       string `json:"sqft"`
-	Price      string `json:"price"`
-	Rent       string `json:"rent"`
+	PropertyID        int    `json:"propertyid"`
+	FullStreetAddress string `json:"address"`
+	City              string `json:"city"`
+	State             string `json:"state"`
+	Zip               string `json:"zip"`
+	Beds              string `json:"beds"`
+	BathsFull         string `json:"bathsfull"`
+	SqFt              string `json:"sqft"`
+	Value             string `json:"value"`
+	Rent              string `json:"rent"`
 }
 
 func main() {
+
 	// Instance of House
 	var house House
 
@@ -32,6 +34,8 @@ func main() {
 	house.PropertyID = GetPropertyID(address, city, state)
 
 	fmt.Println("PropertyID: ", house.PropertyID)
+
+	GetPropertyDetails(strconv.Itoa(house.PropertyID))
 }
 
 func GetPropertyID(address string, city string, state string) int {
@@ -44,8 +48,10 @@ func GetPropertyID(address string, city string, state string) int {
 	req_address := state + "/" + city + "/" + address
 	post_url := fmt.Sprintf(`{"url":"%s"}`, req_address)
 
+	// Body of the request should be a Reader type
 	payload := strings.NewReader(post_url)
 
+	// Creating the request
 	client := &http.Client{}
 	req, err := http.NewRequest(method, url, payload)
 
@@ -53,23 +59,14 @@ func GetPropertyID(address string, city string, state string) int {
 		fmt.Println(err)
 		return 0
 	}
-	req.Header.Add("authority", "www.homesnap.com")
+
+	// Adding headers to the request
 	req.Header.Add("accept", "application/json, text/javascript, */*; q=0.01")
 	req.Header.Add("accept-language", "es-419,es;q=0.9,en-US;q=0.8,en;q=0.7")
 	req.Header.Add("content-type", "application/json; charset=UTF-8")
-	req.Header.Add("cookie", "User=ID=998666696&Hash=0b7701ee4ca214832118018cb0dcd1933d8bd5da; ASP.NET_SessionId=mso5pppcidxlc3i0vjjpt0w0; ASP.NET_SessionId=0ongjuezjzusfhhtiqvpq4h1")
-	req.Header.Add("origin", "https://www.homesnap.com")
-	req.Header.Add("referer", "https://www.homesnap.com/"+req_address)
-	req.Header.Add("sec-ch-ua", "\"Not_A Brand\";v=\"99\", \"Microsoft Edge\";v=\"109\", \"Chromium\";v=\"109\"")
-	req.Header.Add("sec-ch-ua-mobile", "?1")
-	req.Header.Add("sec-ch-ua-platform", "\"Android\"")
-	req.Header.Add("sec-fetch-dest", "empty")
-	req.Header.Add("sec-fetch-mode", "cors")
-	req.Header.Add("sec-fetch-site", "same-origin")
 	req.Header.Add("user-agent", "Mozilla/5.0 (Linux; Android 6.0; Nexus 5 Build/MRA58N) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/109.0.0.0 Mobile Safari/537.36 Edg/109.0.1518.61")
-	req.Header.Add("x-costar-brand", "2")
-	req.Header.Add("x-requested-with", "XMLHttpRequest")
 
+	// Sending Request
 	res, err := client.Do(req)
 	if err != nil {
 		fmt.Println(err)
@@ -103,6 +100,68 @@ func GetPropertyID(address string, city string, state string) int {
 	}
 
 	return propertyId
+}
+
+func GetPropertyDetails(propertyId string) []any {
+
+	// First we connect to the GetByURL endpoint to extract the PropertyID
+	url := "https://www.homesnap.com/service/Properties/GetDetails"
+	method := "POST"
+
+	requestBody := fmt.Sprintf(`{"propertyID":"%s"}`, propertyId)
+
+	// Body of the request should be a Reader type
+	payload := strings.NewReader(requestBody)
+
+	// Creating the request
+	client := &http.Client{}
+	req, err := http.NewRequest(method, url, payload)
+
+	if err != nil {
+		fmt.Println(err)
+		return nil
+	}
+
+	// Adding headers to the request
+	req.Header.Add("accept", "application/json, text/javascript, */*; q=0.01")
+	req.Header.Add("accept-language", "es-419,es;q=0.9,en-US;q=0.8,en;q=0.7")
+	req.Header.Add("content-type", "application/json; charset=UTF-8")
+	req.Header.Add("user-agent", "Mozilla/5.0 (Linux; Android 6.0; Nexus 5 Build/MRA58N) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/109.0.0.0 Mobile Safari/537.36 Edg/109.0.1518.61")
+
+	// Sending Request
+	res, err := client.Do(req)
+	if err != nil {
+		fmt.Println(err)
+		return nil
+	}
+	defer res.Body.Close()
+
+	// Get the response
+	body, err := io.ReadAll(res.Body)
+	if err != nil {
+		fmt.Println(err)
+		return nil
+	}
+
+	// Parse the JSON response
+	var result map[string]interface{}
+	json.Unmarshal([]byte(body), &result)
+
+	// property := result["d"].(map[string]any)
+	// var propertyData []any
+
+	// for key, value := range property {
+	// 	// Each value is an `any` type, that is type asserted as a string
+	// 	if key == "PropertyID" {
+	// 		propertyIdFloat := value.(float64)
+	// 		propertyId = int(propertyIdFloat)
+	// 		// fmt.Println(key, propertyId)
+
+	// 		break
+	// 	}
+	// }
+
+	return []any{}
 }
 
 // fmt.Println(result)
